10a5b9bee2563f1d400b13306cfb6609a5b99ce5
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-02-17 16:26:48 -0300

    Configuração do PWM

--- biossensor.c
+++ biossensor.c
@@ -2,6 +2,11 @@
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/i2c.h"
+#include "hardware/pwm.h"
+#include "lib/ssd1306.h"
+
+#define PWM_FREQ 50
+#define PWM_WRAP 4095
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
@@ -18,7 +23,14 @@
 #define PWM_FREQ 50
 #define PWM_WRAP 4095
 
+// Variáveis globais
+ssd1306_t ssd;
+
 void setup_config(){ 
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, DISPLAY_ADDR, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
 
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
@@ -31,10 +43,18 @@ void setup_config(){
     adc_select_input(0);
 }
 
+void setup_pwm(uint pin) {
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(pin); 
+    pwm_set_wrap(slice, PWM_WRAP);
+    pwm_set_clkdiv(slice, 125.0); 
+    pwm_set_enabled(slice, true);
+}
+
 int main() {
 
     stdio_init_all(); 
-    
+
     setup_config();
 
     while (1) {
