7f4f87b5bbb9d6fe3d14b4432552ca12380ae118
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-02-17 15:52:03 -0300

    Configuração i2c

--- biossensor.c
+++ biossensor.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#include "hardware/i2c.h"
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
@@ -17,12 +18,24 @@
 #define PWM_FREQ 50
 #define PWM_WRAP 4095
 
-int main() {
-    stdio_init_all();  
-    
+void setup_config(){ 
+
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
     adc_init();  
     adc_gpio_init(26); 
     adc_select_input(0);
+}
+
+int main() {
+
+    stdio_init_all(); 
+    
+    setup_config();
 
     while (1) {
         uint16_t leitura_adc = adc_read(); 
