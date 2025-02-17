52cba4d9ac1bf53d1d36d89d87046ce4c833adc7
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-02-17 15:48:34 -0300

    Inicialização do codigo e criação da font e biblioteca do ssd1306.

--- biossensor.c
+++ biossensor.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define DISPLAY_ADDR 0x3C 
 
+#define BIOSSENSOR_OPTICO 26 
+#define JOYSTICK_PB 22
+       
+#define LED_RED 13
+#define LED_GREEN 11
+#define LED_BLUE 12
 
-int main()
-{
-    stdio_init_all();
+#define PWM_FREQ 50
+#define PWM_WRAP 4095
 
-    while (true) {
-        printf("Hello, world!\n");
+int main() {
+    stdio_init_all();  
+    
+    adc_init();  
+    adc_gpio_init(26); 
+    adc_select_input(0);
+
+    while (1) {
+        uint16_t leitura_adc = adc_read(); 
+        float tensao = (leitura_adc * 3.3) / 4095; 
+        // Teste
+        printf("Leitura ADC: %d | Tensão: %.2fV\n", leitura_adc, tensao);
         sleep_ms(1000);
     }
+
+    return 0;
 }
